84161f74ff162daa024d47d86d7912809f42b5e9
JoseGildenilson <josegildenilson@gmail.com>
2026-07-28 14:25:38 -0300

    links atualizados dos dashboard e novo dashboard para o financeiro

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const POLOS = [
     {
         id: 'RN',
         name: 'Rio Grande do Norte',
-        url: 'https://app.powerbi.com/view?r=eyJrIjoiZjc2ZDU5MDEtNTc2Yi00ZjlmLWIwNDUtNDA0OGQ1ODkyZWRmIiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
+        url: 'https://app.powerbi.com/view?r=eyJrIjoiYjI3YjU0MTMtNWZhNC00OTNlLTlhOTctYjk3MmM0ZjU2YzQwIiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
     },
     {
         id: 'PB',
         name: 'Paraíba',
-        url: 'https://app.powerbi.com/view?r=eyJrIjoiZTI3MGE5YzktNDhhMS00MzRkLWI2ZWYtOWI3M2ZhM2RjZjU5IiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
+        url: 'https://app.powerbi.com/view?r=eyJrIjoiNWI0N2ViNDAtYjYxYi00ODI5LWIwOTctMDc4ODBjMDk5ZTJmIiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
     },
     {
         id: 'PR',
         name: 'Paraná',
-        url: 'https://app.powerbi.com/view?r=eyJrIjoiNDhkNTg0NjQtNWMzYS00MTg5LTlkMjgtZGIzZjVkZDgyZWY1IiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
+        url: 'https://app.powerbi.com/view?r=eyJrIjoiNWI1YTY1ZmYtNWYxYi00MmUwLWIxN2YtNDdiZWQzOWE0ZWM3IiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
     },
     {
         id: 'RS',
@@ -22,12 +22,17 @@ const POLOS = [
     {
         id: 'PE',
         name: 'Pernambuco',
-        url: 'https://app.powerbi.com/view?r=eyJrIjoiZmQ4ZjkxNGUtN2IxYi00NmNiLWFiNTYtN2FkY2M0ODkwZGMzIiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
+        url: 'https://app.powerbi.com/view?r=eyJrIjoiZGU4NzUyODYtY2NhMi00MGZmLWIwMTItM2U0MDYyNzYwMDJmIiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
     },
     {
         id: 'F1',
         name: 'Financeiro 1',
         url: 'https://app.powerbi.com/view?r=eyJrIjoiYWUxZDY4ZTktNTVlYy00MzkzLWFjYjItZThlZjBhNDRmM2I3IiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
+    },
+    {
+        id: 'F2',
+        name: 'Financeiro 2',
+        url: 'https://app.powerbi.com/view?r=eyJrIjoiMTM0MGZhNWUtNjk5ZC00YWJlLTkxMjQtNDhjNjUxNWI2OWI0IiwidCI6IjIzMzBlMWVlLTAwYWMtNGVlZi1iNzkzLWU3YzFhMmE0NmU3ZSJ9'
     },
     {
         id: 'GL',
@@ -167,12 +172,6 @@ function init() {
         loader.classList.remove('hidden');
         iframe.classList.remove('loaded');
         
-        if (polo.id === 'F1') {
-            iframe.classList.add('show-bottom-bar');
-        } else {
-            iframe.classList.remove('show-bottom-bar');
-        }
-
         iframe.src = polo.url;
 
         iframe.onload = () => {
